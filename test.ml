4e4aa4d436bc8bebb63d064d3fd9eae5c8a79caf
Akiro <akirophi@protonmail.com>
2021-01-24 20:08:05 +0100

    add test foratleast

--- test.ml
+++ test.ml
@@ -1,31 +1,47 @@
 open Utilities
 open Ounit
+open Flux
+
 
 let%test "assertion" =
     check (fun () -> assertion (fun () -> true))
-
 
 let%test "assertion_false" =
     check (fun () ->
         assertion (fun () -> false)) = false
 
+
 let%test "forall" =
     check (fun () ->
         let x = forall (range 1 10) in
-        assertion (fun () -> x>0))
+        assertion (fun () -> x > 0))
 
 let%test "forall_false" =
     check (fun () ->
         let x = forall (range 1 10) in
-        assertion (fun () -> x<9)) = false
+        assertion (fun () -> x < 9)) = false
 
 
 let%test "forsome" =
     check (fun () ->
         let x = forsome (range 1 10) in
-        assertion (fun () -> x>8))
+        assertion (fun () -> x > 8))
+
+let%test "forsome_false" =
+    check (fun () ->
+        let x = forsome (range 1 10) in
+        assertion (fun () -> x > 10)) = false
 
 
+let%test "foratleast" =
+    check (fun () ->
+        let x = foratleast 3 (range 1 10) in
+        assertion (fun () -> x > 7))
+
+let%test "foratleast_false" =
+    check (fun () ->
+        let x = foratleast 4 (range 1 10) in
+        assertion (fun () -> x > 7)) = false
 
 
 let%test "exemple 1" =
