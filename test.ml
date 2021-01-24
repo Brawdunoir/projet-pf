ebe90ef927085575b7a1d5f9e40f604f11ee80bb
Akiro <akirophi@protonmail.com>
2021-01-24 20:27:14 +0100

    add test exemple 2

--- test.ml
+++ test.ml
@@ -44,12 +44,36 @@ let%test "foratleast_false" =
         assertion (fun () -> x > 7)) = false
 
 
+let%test "assumption" =
+    check (fun () ->
+        let x = forall (range 2 50) in
+        assumption (fun () -> x < 25);
+        assertion (fun () -> x < 25))
+
+
+let%test "assumption_false" =
+    check (fun () ->
+        let x = forall (range 2 50) in
+        assumption (fun () -> x < 25);
+        assertion (fun () -> x < 24)) = false
+
 let%test "exemple 1" =
     let values = (range 2 50) in
     check (fun () ->
-    let a = forall values in
-    let b = forsome values in
-    assumption (fun () -> a <> b);
-    let r = premier a in
-    let predicat = (fun () -> r || (a mod b = 0))
-    in assertion predicat)
+        let a = forall values in
+        let b = forsome values in
+        assumption (fun () -> a <> b);
+        let r = premier a in
+        let predicat = (fun () -> r || (a mod b = 0))
+        in assertion predicat)
+
+
+let%test "exemple 2" =
+    let values = (range 1 50) in
+    check (fun () ->
+        let a = forall values in
+        let b = forall values in
+        let premier_entre_eux x y = (pgcd x y = 1) in
+        assumption (fun () -> premier_entre_eux a b);
+        assertion (fun () -> (pgcd a b) = 1))
+
